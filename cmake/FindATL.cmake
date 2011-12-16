9272fd1c97884e4d05c3cc95ac88e62032692cc2
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2011-12-16 16:39:51 +0000

    Added USE_MFC_TOOLS build option defaulted to OFF
    git-svn-id: svn://0x263a.de/tekuum_src@39 3ae3452d-76a7-4814-9b33-b51e13292e42

--- cmake/FindATL.cmake
+++ cmake/FindATL.cmake
@@ -3,10 +3,24 @@ if(WIN32)
   
 	find_path(ATL_INCLUDE_DIR NAMES atlbase.h
 		HINTS
-		$ENV{PLATFORMSDK_DIR}
-		C:/Program Files/Microsoft Platform SDK
+		$ENV{WINDDK_DIR}
+		C:/WinDDK/7600.16385.1
 		PATH_SUFFIXES
-		include/atl
+		inc/atl71
+		)
+		
+	if(CMAKE_CL_64)
+		set(ATL_LIBPATH_SUFFIX "lib/atl/amd64")
+	else()
+		set(ATL_LIBPATH_SUFFIX "lib/atl/i386")
+	endif()
+	
+	find_path(ATL_LIBRARY_DIR NAMES atl.lib
+		HINTS
+		$ENV{WINDDK_DIR}
+		C:/WinDDK/7600.16385.1
+		PATH_SUFFIXES
+		${ATL_LIBPATH_SUFFIX}
 		)
 
 	# handle the QUIETLY and REQUIRED arguments and set ATL_FOUND to TRUE if
@@ -15,8 +29,9 @@ if(WIN32)
 	find_package_handle_standard_args(ATL 
 		DEFAULT_MSG
 		ATL_INCLUDE_DIR
+		ATL_LIBRARY_DIR
 		)
 
-	mark_as_advanced(ATL_INCLUDE_DIR) # ATL_LIBRARIES)
+	mark_as_advanced(ATL_INCLUDE_DIR ATL_LIBRARY_DIR)
   
 endif(WIN32)
--- cmake/FindMFC.cmake
+++ cmake/FindMFC.cmake
@@ -3,10 +3,31 @@ if(WIN32)
   
 	find_path(MFC_INCLUDE_DIR NAMES afxwin.h
 		HINTS
-		$ENV{PLATFORMSDK_DIR}
-		C:/Program Files/Microsoft Platform SDK
+		$ENV{WINDDK_DIR}
+		C:/WinDDK/7600.16385.1
 		PATH_SUFFIXES
-		include/mfc
+		inc/mfc42
+		)
+		
+	if(CMAKE_CL_64)
+		set(MFC_LIBPATH_SUFFIX "lib/mfc/amd64")
+	else()
+		set(MFC_LIBPATH_SUFFIX "lib/Mfc/i386")
+	endif()
+	
+	# find_library(MFC_MFC42 NAMES mfc42
+		# HINTS 
+		# $ENV{WINDDK_DIR}
+		# C:/WinDDK/7600.16385.1
+		# PATH_SUFFIXES
+		# ${DirectX_LIBPATH_SUFFIX})
+		
+	find_path(MFC_LIBRARY_DIR NAMES mfc42.lib
+		HINTS
+		$ENV{WINDDK_DIR}
+		C:/WinDDK/7600.16385.1
+		PATH_SUFFIXES
+		${MFC_LIBPATH_SUFFIX}
 		)
 
 	# handle the QUIETLY and REQUIRED arguments and set MFC_FOUND to TRUE if
@@ -15,8 +36,9 @@ if(WIN32)
 	find_package_handle_standard_args(MFC 
 		DEFAULT_MSG
 		MFC_INCLUDE_DIR
+		MFC_LIBRARY_DIR
 		)
 
-	mark_as_advanced(MFC_INCLUDE_DIR)
+	mark_as_advanced(MFC_INCLUDE_DIR MFC_LIBRARY_DIR)
   
 endif(WIN32)
