a90629ce0d74ea84a47a32852abd787c6158b0e9
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-01-28 13:59:50 +0000

    Disabled MSVC warnings about local variable is initialized but not referenced.
    git-svn-id: svn://0x263a.de/tekuum_src@98 3ae3452d-76a7-4814-9b33-b51e13292e42

--- idlib/precompiled.h
+++ idlib/precompiled.h
@@ -89,6 +89,10 @@ If you have questions concerning this license or the applicable additional terms
 #pragma warning(disable : 4714)				// function marked as __forceinline not inlined
 #pragma warning(disable : 4996)				// unsafe string operations
 
+// Techyon BEGIN
+#pragma warning(disable : 4189)				// local variable is initialized but not referenced
+// Techyon END
+
 #include <malloc.h>							// no malloc.h on mac or unix
 #include <windows.h>						// for qgl.h
 #undef FindText								// stupid namespace poluting Microsoft monkeys
