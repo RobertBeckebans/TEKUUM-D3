a24bdf7d326be8ed18b8a6b59d124beef4fdea9a
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-01-28 20:41:17 +0000

    Added lightmap texcoords to idDrawVert.
    git-svn-id: svn://0x263a.de/tekuum_src@107 3ae3452d-76a7-4814-9b33-b51e13292e42

--- idlib/geometry/DrawVert.h
+++ idlib/geometry/DrawVert.h
@@ -44,11 +44,15 @@ public:
 	idVec3			normal;
 	idVec3			tangents[2];
 	byte			color[4];
+#if defined(USE_LIGHTMAPS)
+	idVec2			lm;
+//	float			padding[2];
+#endif
 #if 0 // was MACOS_X see comments concerning DRAWVERT_PADDED in Simd_Altivec.h 
 	float			padding;
 #endif
-	float			operator[]( const int index ) const;
-	float &			operator[]( const int index );
+//	float			operator[]( const int index ) const;
+//	float &			operator[]( const int index );
 
 	void			Clear( void );
 
@@ -60,15 +64,24 @@ public:
 	void			SetColor( dword color );
 	dword			GetColor( void ) const;
 };
-
+/*
 ID_INLINE float idDrawVert::operator[]( const int index ) const {
+#if defined(USE_LIGHTMAPS)
+	assert( index >= 0 && index < 6 );
+#else
 	assert( index >= 0 && index < 5 );
+#endif
 	return ((float *)(&xyz))[index];
 }
 ID_INLINE float	&idDrawVert::operator[]( const int index ) {
+#if defined(USE_LIGHTMAPS)
+	assert( index >= 0 && index < 6 );
+#else
 	assert( index >= 0 && index < 5 );
+#endif
 	return ((float *)(&xyz))[index];
 }
+*/
 
 ID_INLINE void idDrawVert::Clear( void ) {
 	xyz.Zero();
@@ -77,11 +90,17 @@ ID_INLINE void idDrawVert::Clear( void ) {
 	tangents[0].Zero();
 	tangents[1].Zero();
 	color[0] = color[1] = color[2] = color[3] = 0;
+#if defined(USE_LIGHTMAPS)
+	lm.Zero();
+#endif
 }
 
 ID_INLINE void idDrawVert::Lerp( const idDrawVert &a, const idDrawVert &b, const float f ) {
 	xyz = a.xyz + f * ( b.xyz - a.xyz );
 	st = a.st + f * ( b.st - a.st );
+#if defined(USE_LIGHTMAPS)
+	lm = a.lm + f * ( b.lm - a.lm );
+#endif
 }
 
 ID_INLINE void idDrawVert::LerpAll( const idDrawVert &a, const idDrawVert &b, const float f ) {
@@ -94,6 +113,9 @@ ID_INLINE void idDrawVert::LerpAll( const idDrawVert &a, const idDrawVert &b, co
 	color[1] = (byte)( a.color[1] + f * ( b.color[1] - a.color[1] ) );
 	color[2] = (byte)( a.color[2] + f * ( b.color[2] - a.color[2] ) );
 	color[3] = (byte)( a.color[3] + f * ( b.color[3] - a.color[3] ) );
+#if defined(USE_LIGHTMAPS)
+	lm = a.lm + f * ( b.lm - a.lm );
+#endif
 }
 
 ID_INLINE void idDrawVert::SetColor( dword color ) {
