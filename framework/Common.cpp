e5bb6cdf67e9224c80611369191bac4bf4884347
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-02-06 14:35:20 +0000

    Implemented ProgressDialog for engine startup.
    git-svn-id: svn://0x263a.de/tekuum_src@126 3ae3452d-76a7-4814-9b33-b51e13292e42

--- framework/Common.cpp
+++ framework/Common.cpp
@@ -2490,11 +2490,16 @@ void idCommonLocal::PrintLoadingMessage( const char *msg ) {
 	if ( !( msg && *msg ) ) {
 		return;
 	}
+
+#if defined(__ANDROID__)
+	ji.ShowProgressDialog(msg);
+#else
 	renderSystem->BeginFrame( renderSystem->GetScreenWidth(), renderSystem->GetScreenHeight() );
 	renderSystem->DrawStretchPic( 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 1, 1, declManager->FindMaterial( "splashScreen" ) );
 	int len = strlen( msg );
 	renderSystem->DrawSmallStringExt( ( 640 - len * SMALLCHAR_WIDTH ) / 2, 410, msg, idVec4( 0.0f, 0.81f, 0.94f, 1.0f ), true, declManager->FindMaterial( "textures/bigchars" ) );
 	renderSystem->EndFrame( NULL, NULL );
+#endif
 }
 
 /*
@@ -3126,6 +3131,10 @@ void idCommonLocal::InitGame( void ) {
 
 	// init the session
 	session->Init();
+
+#if defined(__ANDROID__)
+	ji.HideProgressDialog();
+#endif
 
 	// have to do this twice.. first one sets the correct r_mode for the renderer init
 	// this time around the backend is all setup correct.. a bit fugly but do not want
