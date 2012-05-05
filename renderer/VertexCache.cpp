62db0994b45022fe18715700a20a548f2eb62f8f
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-05-05 10:51:51 +0000

    Fixed crash bug if the OpenGL driver does not support GL_ARB_vertex_buffer_object.
    git-svn-id: svn://0x263a.de/tekuum_src@204 3ae3452d-76a7-4814-9b33-b51e13292e42

--- renderer/VertexCache.cpp
+++ renderer/VertexCache.cpp
@@ -253,6 +253,12 @@ void idVertexCache::Alloc( void *data, int size, vertCache_t **buffer, bool inde
 			if( !virtualMemory ) {
 				glGenBuffers( 1, & block->vbo );
 			}
+			// Techyon RB: block->vbo != 0 is possible with bad drivers which don't support GL_ARB_vertex_buffer_object
+			else
+			{
+				block->vbo = 0;
+			}
+			// Techyon END
 		}
 	}
 
