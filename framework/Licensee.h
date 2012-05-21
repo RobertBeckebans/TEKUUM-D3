530c1342269db5945475dec23ac760a0cb43439e
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-05-21 12:12:27 +0000

    Renamed Doom3 to Techyon in Licensee.h
    git-svn-id: svn://0x263a.de/tekuum_src@220 3ae3452d-76a7-4814-9b33-b51e13292e42

--- framework/Licensee.h
+++ framework/Licensee.h
@@ -34,12 +34,12 @@ If you have questions concerning this license or the applicable additional terms
 ===============================================================================
 */
 
-#define GAME_NAME						"DOOM 3"		// appears on window titles and errors
+#define GAME_NAME						"Techyon"		// appears on window titles and errors
 
-#define ENGINE_VERSION					"DOOM 1.3.1"	// printed in console
+#define ENGINE_VERSION					"Techyon 0.3.0"	// printed in console
 
 // paths
-#define	CD_BASEDIR						"Doom"
+#define	CD_BASEDIR						"Techyon"
 #ifdef ID_DEMO_BUILD
 	#define BASE_GAMEDIR					"demo"
 #else
@@ -47,8 +47,8 @@ If you have questions concerning this license or the applicable additional terms
 #endif
 
 // filenames
-#define	CD_EXE							"doom.exe"
-#define CONFIG_FILE						"DoomConfig.cfg"
+#define	CD_EXE							"techyon.exe"
+#define CONFIG_FILE						"TechyonConfig.cfg"
 
 // base folder where the source code lives
 #define SOURCE_CODE_BASE_FOLDER			"src"
@@ -89,25 +89,25 @@ If you have questions concerning this license or the applicable additional terms
 
 // editor info
 #define EDITOR_DEFAULT_PROJECT			"doom.qe4"
-#define EDITOR_REGISTRY_KEY				"DOOMRadiant"
-#define EDITOR_WINDOWTEXT				"DOOMEdit"
+#define EDITOR_REGISTRY_KEY				"TechyonRadiant"
+#define EDITOR_WINDOWTEXT				"TechyonEdit"
 
 // win32 info
-#define WIN32_CONSOLE_CLASS				"DOOM 3 WinConsole"
-#define	WIN32_WINDOW_CLASS_NAME			"DOOM3"
-#define	WIN32_FAKE_WINDOW_CLASS_NAME	"DOOM3_WGL_FAKE"
+#define WIN32_CONSOLE_CLASS				"TECHYON WinConsole"
+#define	WIN32_WINDOW_CLASS_NAME			"TECHYON"
+#define	WIN32_FAKE_WINDOW_CLASS_NAME	"TECHYON_WGL_FAKE"
 
 // Linux info
 #ifdef ID_DEMO_BUILD
-	#define LINUX_DEFAULT_PATH			"/usr/local/games/doom3-demo"
+	#define LINUX_DEFAULT_PATH			"/usr/local/games/techyon-demo"
 #else
-	#define LINUX_DEFAULT_PATH			"/usr/local/games/doom3"
+	#define LINUX_DEFAULT_PATH			"/usr/local/games/techyon"
 #endif
 
 // CD Key file info
 // goes into BASE_GAMEDIR whatever the fs_game is set to
 // two distinct files for easier win32 installer job
-#define CDKEY_FILE						"doomkey"
+#define CDKEY_FILE						"techyonkey"
 #define XPKEY_FILE						"xpkey"
 #define CDKEY_TEXT						"\n// Do not give this file to ANYONE.\n" \
 										"// id Software or Zenimax will NEVER ask you to send this file to them.\n"
