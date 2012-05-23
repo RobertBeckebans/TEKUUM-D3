0984006991fc967ab73247cb1a14e77fb9e273e2
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-05-23 16:17:35 +0000

    Fixed for loop in idPlayer::UpdateHud()
    git-svn-id: svn://0x263a.de/tekuum_src@233 3ae3452d-76a7-4814-9b33-b51e13292e42

--- game/Player.cpp
+++ game/Player.cpp
@@ -6058,7 +6058,9 @@ void idPlayer::UpdateHud( void ) {
 				inventory.nextItemNum = 1;
 			}
 			int i;
-			for ( i = 0; i < 5, i < c; i++ ) {
+			// Techyon RB: fixed left operand of comma operator has no effect
+			for ( i = 0; i < 5 && i < c; i++ ) {
+			// Techyon END
 				hud->SetStateString( va( "itemtext%i", inventory.nextItemNum ), inventory.pickupItemNames[0].name );
 				hud->SetStateString( va( "itemicon%i", inventory.nextItemNum ), inventory.pickupItemNames[0].icon );
 				hud->HandleNamedEvent( va( "itemPickup%i", inventory.nextItemNum++ ) );
