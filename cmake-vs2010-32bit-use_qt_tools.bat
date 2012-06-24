3c71682487e1e2fa1811250b3cdeea2fa4f1feb4
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-06-24 12:23:48 +0000

    Tweaked default CMake options.
    git-svn-id: svn://0x263a.de/tekuum_src@254 3ae3452d-76a7-4814-9b33-b51e13292e42

--- cmake-vs2010-32bit-use_qt_tools.bat
+++ cmake-vs2010-32bit-use_qt_tools.bat
@@ -2,5 +2,6 @@ cd ..
 del /s /q build
 mkdir build
 cd build
-cmake -G "Visual Studio 10" -DCMAKE_USE_RELATIVE_PATHS=ON -DUSE_QT_TOOLS=ON ../src
+REM cmake -G "Visual Studio 10" -DCMAKE_USE_RELATIVE_PATHS=ON -DUSE_QT_TOOLS=ON -DUSE_QT_WINDOWING=ON ../src
+cmake -G "Visual Studio 10" -DCMAKE_USE_RELATIVE_PATHS=ON -DUSE_QT_TOOLS=ON -DMONOLITH=ON ../src
 pause
