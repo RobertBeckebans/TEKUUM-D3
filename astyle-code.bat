51b05a746e0541d7c59db0be4b396f1164101643
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-06-25 20:35:11 +0000

    Tweaked AStyle options.
    git-svn-id: svn://0x263a.de/tekuum_src@257 3ae3452d-76a7-4814-9b33-b51e13292e42

--- astyle-code.bat
+++ astyle-code.bat
@@ -1,12 +1,4 @@
-REM cd ../Bin/Win32
-REM ARTISTIC_STYLE_OPTIONS=
-REM OPTIONS="--style=allman --indent=tab=4"
-
-REM AStyle.exe --options=indent-options.ini game\Game.cpp
-REM AStyle.exe --options=indent-options.ini Game/Game.h
-REM AStyle.exe --options=indent-options.ini --recursive *.cpp
-REM AStyle.exe --options=indent-options.ini --exclude="cm framework game libs renderer sound sys tools ui" --recursive *.h
-
-astyle.exe -v --options=astyle-options.ini --exclude="libs" --recursive *.h
-astyle.exe -v --options=astyle-options.ini --exclude="libs" --exclude="idlib/math/Simd_3DNow.cpp" --exclude="idlib/math/Simd_AltiVec.cpp" --exclude="idlib/math/Simd_MMX.cpp" --exclude="idlib/math/Simd_SSE.cpp" --exclude="idlib/math/Simd_SSE2.cpp" --exclude="idlib/math/Simd_SSE3.cpp" --recursive *.cpp
+REM ..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --exclude="libs" --exclude="cm" --exclude="framework" --exclude="game" --exclude="idlib" --exclude="MayaImport" --exclude="renderer" --exclude="sound" --exclude="sys" --exclude="tools" --exclude="TypeInfo" --recursive *.h
+..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --exclude="libs" --recursive *.h
+..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --exclude="libs" --exclude="idlib/math/Simd_3DNow.cpp" --exclude="idlib/math/Simd_AltiVec.cpp" --exclude="idlib/math/Simd_MMX.cpp" --exclude="idlib/math/Simd_SSE.cpp" --exclude="idlib/math/Simd_SSE2.cpp" --exclude="idlib/math/Simd_SSE3.cpp" --recursive *.cpp
 pause
