4e50831b4955c8bb27649d7efc050a224f715a93
trebor <trebor@3ae3452d-76a7-4814-9b33-b51e13292e42>
2012-06-25 21:33:30 +0000

    Tweaked AStyle options.
    git-svn-id: svn://0x263a.de/tekuum_src@258 3ae3452d-76a7-4814-9b33-b51e13292e42

--- astyle-code.bat
+++ astyle-code.bat
@@ -1,4 +1,6 @@
-REM ..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --exclude="libs" --exclude="cm" --exclude="framework" --exclude="game" --exclude="idlib" --exclude="MayaImport" --exclude="renderer" --exclude="sound" --exclude="sys" --exclude="tools" --exclude="TypeInfo" --recursive *.h
-..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --exclude="libs" --recursive *.h
-..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --exclude="libs" --exclude="idlib/math/Simd_3DNow.cpp" --exclude="idlib/math/Simd_AltiVec.cpp" --exclude="idlib/math/Simd_MMX.cpp" --exclude="idlib/math/Simd_SSE.cpp" --exclude="idlib/math/Simd_SSE2.cpp" --exclude="idlib/math/Simd_SSE3.cpp" --recursive *.cpp
+REM ..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --exclude="libs" --recursive *.h
+REM ..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --exclude="libs" --exclude="idlib/math/Simd_3DNow.cpp" --exclude="idlib/math/Simd_AltiVec.cpp" --exclude="idlib/math/Simd_MMX.cpp" --exclude="idlib/math/Simd_SSE.cpp" --exclude="idlib/math/Simd_SSE2.cpp" --exclude="idlib/math/Simd_SSE3.cpp" --exclude="sys/win32/win_cpu.cpp" --exclude="sys/win32/win_main.cpp" --exclude="sys/win32/win_shared.cpp" --exclude="sys/android/android_main.cpp" --exclude="sys/linux/main.cpp" --recursive *.cpp
+
+..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --recursive ui/*.h
+..\bin\win32\UniversalIndentGUI\indenters\astyle.exe -v --options=astyle-options.ini --recursive ui/*.cpp
 pause
