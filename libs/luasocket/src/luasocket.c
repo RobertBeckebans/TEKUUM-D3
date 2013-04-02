5b5da0ac7ebfffe2070a2e0ed979a9bbf40f569e
Robert Beckebans <robert.beckebans@gmail.com>
2013-04-03 00:05:42 +0200

    Small fix to allow loading of socket.lua additionally to socket.core

--- libs/luasocket/src/luasocket.c
+++ libs/luasocket/src/luasocket.c
@@ -89,7 +89,9 @@ static int global_unload(lua_State *L) {
 static int base_open(lua_State *L) {
     if (socket_open()) {
         /* export functions (and leave namespace table on top of stack) */
-        luaL_openlib(L, "socket", func, 0);
+		// RB: changed "socket" to "socket.core"
+        luaL_openlib(L, "socket.core", func, 0);
+		// RB end
 #ifdef LUASOCKET_DEBUG
         lua_pushstring(L, "_DEBUG");
         lua_pushboolean(L, 1);
