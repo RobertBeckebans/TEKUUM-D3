9119dccc88d4c5660789107548665dad39fdb613
Robert Beckebans <robert.beckebans@gmail.com>
2013-04-09 13:51:32 +0200

    Fixed missing time updates for playing .roq videos

--- renderer/GuiModel.cpp
+++ renderer/GuiModel.cpp
@@ -303,6 +303,12 @@ void idGuiModel::EmitFullScreen()
 	viewDef->drawSurfs = ( drawSurf_t** )R_FrameAlloc( viewDef->maxDrawSurfs * sizeof( viewDef->drawSurfs[0] ), FRAME_ALLOC_DRAW_SURFACE_POINTER );
 	viewDef->numDrawSurfs = 0;
 	
+	// RB: give renderView the current time to calculate 2D shader effects
+	int shaderTime = tr.frameShaderTime * 1000; //Sys_Milliseconds();
+	viewDef->renderView.time[0] = shaderTime;
+	viewDef->renderView.time[1] = shaderTime;
+	// RB end
+	
 	viewDef_t* oldViewDef = tr.viewDef;
 	tr.viewDef = viewDef;
 	
