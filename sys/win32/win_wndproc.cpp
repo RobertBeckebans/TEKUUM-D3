6415365d3b80c88ae6c2db7a6cde0871fc0bae69
Robert Beckebans <robert.beckebans@gmail.com>
2013-04-20 22:57:07 +0200

    Merged win_wndproc.cpp for resizing the window

--- sys/win32/win_wndproc.cpp
+++ sys/win32/win_wndproc.cpp
@@ -1,40 +1,48 @@
 /*
 ===========================================================================
 
-Doom 3 GPL Source Code
-Copyright (C) 1999-2011 id Software LLC, a ZeniMax Media company.
+Doom 3 BFG Edition GPL Source Code
+Copyright (C) 1993-2012 id Software LLC, a ZeniMax Media company.
+Copyright (C) 2012 Robert Beckebans
 
-This file is part of the Doom 3 GPL Source Code (?Doom 3 Source Code?).
+This file is part of the Doom 3 BFG Edition GPL Source Code ("Doom 3 BFG Edition Source Code").
 
-Doom 3 Source Code is free software: you can redistribute it and/or modify
+Doom 3 BFG Edition Source Code is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Doom 3 Source Code is distributed in the hope that it will be useful,
+Doom 3 BFG Edition Source Code is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Doom 3 Source Code.  If not, see <http://www.gnu.org/licenses/>.
+along with Doom 3 BFG Edition Source Code.  If not, see <http://www.gnu.org/licenses/>.
 
-In addition, the Doom 3 Source Code is also subject to certain additional terms. You should have received a copy of these additional terms immediately following the terms and conditions of the GNU General Public License which accompanied the Doom 3 Source Code.  If not, please request a copy in writing from id Software at the address below.
+In addition, the Doom 3 BFG Edition Source Code is also subject to certain additional terms. You should have received a copy of these additional terms immediately following the terms and conditions of the GNU General Public License which accompanied the Doom 3 BFG Edition Source Code.  If not, please request a copy in writing from id Software at the address below.
 
 If you have questions concerning this license or the applicable additional terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 
 ===========================================================================
 */
 
-#include "precompiled.h"
 #pragma hdrstop
+#include "precompiled.h"
 
 #include "win_local.h"
 #include "../../renderer/tr_local.h"
 
+#include <windowsx.h>
+
 LONG WINAPI MainWndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
 
 static bool s_alttab_disabled;
+
+extern idCVar r_windowX;
+extern idCVar r_windowY;
+extern idCVar r_windowWidth;
+extern idCVar r_windowHeight;
 
 static void WIN_DisableAltTab()
 {
@@ -50,7 +58,13 @@ static void WIN_DisableAltTab()
 	{
 		BOOL old;
 		
+		// RB begin
+#if defined(__MINGW32__)
+		SystemParametersInfo( SPI_GETSCREENSAVEACTIVE, 1, &old, 0 );
+#else
 		SystemParametersInfo( SPI_SCREENSAVERRUNNING, 1, &old, 0 );
+#endif
+		// RB end
 	}
 	s_alttab_disabled = true;
 }
@@ -69,7 +83,13 @@ static void WIN_EnableAltTab()
 	{
 		BOOL old;
 		
-		SystemParametersInfo( SPI_SCREENSAVERRUNNING, 0, &old, 0 );
+		// RB begin
+#if defined(__MINGW32__)
+		SystemParametersInfo( SPI_GETSCREENSAVEACTIVE, 1, &old, 0 );
+#else
+		SystemParametersInfo( SPI_SCREENSAVERRUNNING, 1, &old, 0 );
+#endif
+		// RB end
 	}
 	
 	s_alttab_disabled = false;
@@ -82,11 +102,7 @@ void WIN_Sizing( WORD side, RECT* rect )
 		return;
 	}
 	
-	if( idKeyInput::IsDown( K_CTRL ) )
-	{
-		return;
-	}
-	
+	// restrict to a standard aspect ratio
 	int width = rect->right - rect->left;
 	int height = rect->bottom - rect->top;
 	
@@ -100,46 +116,49 @@ void WIN_Sizing( WORD side, RECT* rect )
 	height -= decoHeight;
 	
 	// Clamp to a minimum size
-	int minWidth = 160;
-	int minHeight = minWidth * SCREEN_HEIGHT / SCREEN_WIDTH;
+	if( width < SCREEN_WIDTH / 4 )
+	{
+		width = SCREEN_WIDTH / 4;
+	}
+	if( height < SCREEN_HEIGHT / 4 )
+	{
+		height = SCREEN_HEIGHT / 4;
+	}
 	
-	if( width < minWidth )
-	{
-		width = minWidth;
-	}
-	if( height < minHeight )
-	{
-		height = minHeight;
-	}
+	const int minWidth = height * 4 / 3;
+	const int maxHeight = width * 3 / 4;
+	
+	const int maxWidth = height * 16 / 9;
+	const int minHeight = width * 9 / 16;
 	
 	// Set the new size
 	switch( side )
 	{
 		case WMSZ_LEFT:
 			rect->left = rect->right - width - decoWidth;
-			rect->bottom = rect->top + ( width * SCREEN_HEIGHT / SCREEN_WIDTH ) + decoHeight;
+			rect->bottom = rect->top + idMath::ClampInt( minHeight, maxHeight, height ) + decoHeight;
 			break;
 		case WMSZ_RIGHT:
 			rect->right = rect->left + width + decoWidth;
-			rect->bottom = rect->top + ( width * SCREEN_HEIGHT / SCREEN_WIDTH ) + decoHeight;
+			rect->bottom = rect->top + idMath::ClampInt( minHeight, maxHeight, height ) + decoHeight;
 			break;
 		case WMSZ_BOTTOM:
 		case WMSZ_BOTTOMRIGHT:
 			rect->bottom = rect->top + height + decoHeight;
-			rect->right = rect->left + ( height * SCREEN_WIDTH / SCREEN_HEIGHT ) + decoWidth;
+			rect->right = rect->left + idMath::ClampInt( minWidth, maxWidth, width ) + decoWidth;
 			break;
 		case WMSZ_TOP:
 		case WMSZ_TOPRIGHT:
 			rect->top = rect->bottom - height - decoHeight;
-			rect->right = rect->left + ( height * SCREEN_WIDTH / SCREEN_HEIGHT ) + decoWidth;
+			rect->right = rect->left + idMath::ClampInt( minWidth, maxWidth, width ) + decoWidth;
 			break;
 		case WMSZ_BOTTOMLEFT:
 			rect->bottom = rect->top + height + decoHeight;
-			rect->left = rect->right - ( height * SCREEN_WIDTH / SCREEN_HEIGHT ) - decoWidth;
+			rect->left = rect->right - idMath::ClampInt( minWidth, maxWidth, width ) - decoWidth;
 			break;
 		case WMSZ_TOPLEFT:
 			rect->top = rect->bottom - height - decoHeight;
-			rect->left = rect->right - ( height * SCREEN_WIDTH / SCREEN_HEIGHT ) - decoWidth;
+			rect->left = rect->right - idMath::ClampInt( minWidth, maxWidth, width ) - decoWidth;
 			break;
 	}
 }
@@ -305,12 +324,47 @@ LONG WINAPI MainWndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
 				RECT rect;
 				if( ::GetClientRect( win32.hWnd, &rect ) )
 				{
-					glConfig.nativeScreenWidth = rect.right - rect.left;
-					glConfig.nativeScreenHeight = rect.bottom - rect.top;
+				
+					if( rect.right > rect.left && rect.bottom > rect.top )
+					{
+						glConfig.nativeScreenWidth = rect.right - rect.left;
+						glConfig.nativeScreenHeight = rect.bottom - rect.top;
+						
+						// save the window size in cvars if we aren't fullscreen
+						int style = GetWindowLong( hWnd, GWL_STYLE );
+						if( ( style & WS_POPUP ) == 0 )
+						{
+							r_windowWidth.SetInteger( glConfig.nativeScreenWidth );
+							r_windowHeight.SetInteger( glConfig.nativeScreenHeight );
+						}
+					}
 				}
 			}
 			break;
+		case WM_MOVE:
+		{
+			int		xPos, yPos;
+			RECT r;
 			
+			// save the window origin in cvars if we aren't fullscreen
+			int style = GetWindowLong( hWnd, GWL_STYLE );
+			if( ( style & WS_POPUP ) == 0 )
+			{
+				xPos = ( short ) LOWORD( lParam ); // horizontal position
+				yPos = ( short ) HIWORD( lParam ); // vertical position
+				
+				r.left   = 0;
+				r.top    = 0;
+				r.right  = 1;
+				r.bottom = 1;
+				
+				AdjustWindowRect( &r, style, FALSE );
+				
+				r_windowX.SetInteger( xPos + r.left );
+				r_windowY.SetInteger( yPos + r.top );
+			}
+			break;
+		}
 		case WM_CREATE:
 		
 			win32.hWnd = hWnd;
@@ -360,11 +414,19 @@ LONG WINAPI MainWndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
 				idKeyInput::ClearStates();
 				com_editorActive = false;
 				Sys_GrabMouseCursor( true );
+				if( common->IsInitialized() )
+				{
+					SetCursor( NULL );
+				}
 			}
 			
 			if( fActive == WA_INACTIVE )
 			{
 				win32.movingWindow = false;
+				if( common->IsInitialized() )
+				{
+					SetCursor( LoadCursor( 0, IDC_ARROW ) );
+				}
 			}
 			
 			// start playing the game sound world
@@ -374,33 +436,6 @@ LONG WINAPI MainWndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
 			// that will be done next time through the main loop
 		}
 		break;
-		
-		case WM_MOVE:
-		{
-			int		xPos, yPos;
-			RECT r;
-			int		style;
-			
-			if( !win32.cdsFullscreen )
-			{
-				xPos = ( short ) LOWORD( lParam ); // horizontal position
-				yPos = ( short ) HIWORD( lParam ); // vertical position
-				
-				r.left   = 0;
-				r.top    = 0;
-				r.right  = 1;
-				r.bottom = 1;
-				
-				style = GetWindowLong( hWnd, GWL_STYLE );
-				AdjustWindowRect( &r, style, FALSE );
-				
-				win32.win_xpos.SetInteger( xPos + r.left );
-				win32.win_ypos.SetInteger( yPos + r.top );
-				win32.win_xpos.ClearModified();
-				win32.win_ypos.ClearModified();
-			}
-			break;
-		}
 		case WM_TIMER:
 		{
 			if( win32.win_timerUpdate.GetBool() )
